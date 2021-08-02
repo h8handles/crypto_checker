94364b0c6d79f7bfc9435b1de189ee367f5c5369
h8handles <h8handles@gmail.com>
2021-08-02 15:52:37 -0400

    start repo

---
+++ main.py
@@ -0,0 +1,18 @@
+
+
+#import requests as r
+import keys
+import requests as r
+import json
+from os import write
+
+
+api_url = 'https://api.nomics.com/v1/markets?key=' + keys.key
+
+output = r.get(api_url)
+
+data = output.text
+datFile = open('data_out.json', '+w')
+
+datFile.write(data)
+print(datFile.read())
