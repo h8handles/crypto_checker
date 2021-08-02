e9256de3442453f733b25f3dd7eb82ca222cd0e5
h8handles <h8handles@gmail.com>
2021-08-02 16:11:11 -0400

    made the method for retrival

--- main.py
+++ main.py
@@ -7,12 +7,12 @@ import json
 from os import write
 
 
-api_url = 'https://api.nomics.com/v1/markets?key=' + keys.key
+def get_crypt():
+    api_url = 'https://api.nomics.com/v1/markets?key=' + keys.key
+    output = r.get(api_url)
+    data = output.text
+    datFile = open('data_out.json', '+w')
+    datFile.write(data)
 
-output = r.get(api_url)
 
-data = output.text
-datFile = open('data_out.json', '+w')
-
-datFile.write(data)
-print(datFile.read())
+get_crypt()
